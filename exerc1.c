0c0e1fc808e0317db99a53e59a96a9d98c73a4fd
alasky-sketch <133282543+alasky-sketch@users.noreply.github.com>
2025-09-21 21:05:22 -0300

    exerc1.c

--- exerc1.c
+++ exerc1.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*Primeiro foi declarado o array juntamente da variavel de controle dos loops e o tamanho do array*/
-/*Em seguida, utilizando a funcao mallloc, um espaço na memoria foi alocado para o vetor de 5 numeros inteiros e com o argumento (tamanho * sizeof(int) e retornado o tamannho em bytes*/
-/*Entao, e feita uma verificacao se a memoria foi alocada com sucesso*/
-/*Depois disso, é feito um loop para que o usuario digite os cinco numeros que foram reservados um espaco na memoria*/
+/*Primeiro,foi declarado o array juntamente da variável de controle dos loops e o tamanho do array*/
+/*Em seguida, utilizando a funçao mallloc, um espaço na memória foi alocado para o vetor de 5 numeros inteiros e com o argumento (tamanho * sizeof(int) é retornado o tamannho em bytes*/
+/*Entao, é feita uma verificação se a memoria foi alocada com sucesso*/
+/*Depois disso, é pedido para o usuário que digitasse cinco numeros*/
 /*Após a leitura, os números inseridos são exibidos na tela*/
 /*Por fim, a memória alocada dinamicamente é liberada com a função free*/
 int main() {
@@ -28,3 +28,4 @@ int main() {
     free(a);
     return 0;
 }
+
